1c66e390871d4d38e38e5dd7b295bacedc86debd
Minik Olsen <minikolsen9@gmail.com>
2016-02-20 10:00:12 -0200

    Fixing dependency on new pip.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         'youtube-dl',
         'beautifulsoup4',
-        'gevent>1.0.2',
+        'gevent==1.1b6',
         'grequests'],
     dependency_links=['https://github.com/rg3/youtube-dl/'],
     classifiers=[
